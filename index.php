4cf74789be11a744d60d24bbf670b9ca17fb6fd0
Amin PO Muar <helmi007@yahoo.com>
2015-09-30 14:52:08 +0800

    guna # untuk komen biasa , guna // untuk tutup koding php

--- index.php
+++ index.php
@@ -9,17 +9,17 @@
  * 5. istihar class Mulakan
  */
  
-// 1. laporan tahap kesilapan kod PHP
+# 1. laporan tahap kesilapan kod PHP
 error_reporting(E_ALL);
  
-// 2. isytiharkan zon masa => Asia/Kuala Lumpur
+# 2. isytiharkan zon masa => Asia/Kuala Lumpur
 date_default_timezone_set('Asia/Kuala_Lumpur');
  
-// 3. setkan tatarajah sistem
+# 3. setkan tatarajah sistem
 require 'tatarajah.php';
 
-// 4. masukkan semua fail class dari folder PUSTAKA
-//    Also spl_autoload_register (Take a look at it if you like)
+# 4. masukkan semua fail class dari folder PUSTAKA
+#    Also spl_autoload_register (Take a look at it if you like)
 function __autoload($class)
 {
 	//echo '$class=' .$class . '<hr>';
@@ -29,5 +29,5 @@ function __autoload($class)
 	require $cariFail[0];
 }
 
-// 5. istihar class Mulakan
+# 5. istihar class Mulakan
 $aplikasi = new Mulakan();
