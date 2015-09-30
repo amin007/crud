c20d1b29024aab8930c27ad4bf24249d5b954725
Amin PO Muar <helmi007@yahoo.com>
2015-09-30 21:38:33 +0800

    Tambah fail Kawal.php

---
+++ aplikasi/pustaka/Kawal.php
@@ -0,0 +1,41 @@
+<?php
+
+class Kawal 
+{
+
+	function __construct() 
+	{
+		//echo '<br>class Kawal';
+		$this->papar = new Papar();
+	}
+	
+	public function muatTanya($nama) 
+	{
+		
+        $failTanya = GetMatchingFiles(GetContents(TANYA),$nama . '_tanya.php');
+		$path = $failTanya[0];
+		//$path = TANYA . $nama . '_tanya.php';
+		//echo '<br> class Kawal :: $nama : ' . $nama . '|';
+		//echo 'TANYA->' . TANYA . '|';
+		//echo 'senarai fail -><pre>'; print_r(GetContents(TANYA)) . '</pre>|';
+		//echo '$failTanya->'; print_r($failTanya) . '|';
+		//echo '$path->' . $path . '<br>';
+		//$tanyaNAMA = ucfirst($nama) . '_Tanya';
+		//echo '<br>$tanyaNAMA->' . $tanyaNAMA . '<br>';
+		
+		if (file_exists($path)) 
+		{
+			$tanyaNama = ucfirst($nama) . '_Tanya';
+			//echo '<br>$tanyaNama->' . $tanyaNama . '<br>';
+			
+			require_once $path;
+			$this->tanya = new $tanyaNama();
+			/*
+			if (class_exists($tanyaNama)) echo '<br>class ' . $tanyaNama . ' wujud<br>';
+			else echo '<br>class ' . $tanyaNama . ' tak wujud<br>';
+			*/
+		}
+					
+	}
+
+}
