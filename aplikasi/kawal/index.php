7bbbe700fe35d9fece30621e57fb3bff1766ab8e
Amin PO Muar <helmi007@yahoo.com>
2015-09-30 20:59:36 +0800

    tambah fail kawal/index.php

---
+++ aplikasi/kawal/index.php
@@ -0,0 +1,22 @@
+<?php
+
+class Index extends Kawal 
+{
+#--------------------------------------------------------------------------------------#
+	function __construct() 
+	{
+		echo '<br>class Index extends Kawal';
+		parent::__construct();
+        Kebenaran::kawalMasuk();
+	}
+	
+	function index() 
+	{
+		echo '<br>class Index::index() extends Kawal';
+		# Set pemboleubah utama
+		$this->papar->Tajuk_Muka_Surat='Enjin Carian Ekonomi';
+		# pergi papar kandungan
+		//$this->papar->baca('index/index');
+	}
+#--------------------------------------------------------------------------------------#	
+}
