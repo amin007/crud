5443dfe69638227a560da9541c328c8e1bc6d2ff
Amin PO Muar <helmi007@yahoo.com>
2015-09-30 21:02:04 +0800

    tambah fail kawal/sesat.php

---
+++ aplikasi/kawal/sesat.php
@@ -0,0 +1,32 @@
+<?php
+
+class Sesat extends Kawal 
+{
+#--------------------------------------------------------------------------------------#
+	function __construct() 
+	{
+		parent::__construct();
+	}
+	
+	function index() 
+	{
+		echo '<br>class Sesat::index() extends Kawal';
+		$this->papar->mesej = 'Halaman ini tidak wujud';
+		//$this->papar->baca('sesat/index');
+	}
+
+	function parameter() 
+	{
+		echo '<br>class Sesat::parameter() extends Kawal';
+		$this->papar->mesej = 'Class wujud tapi parameter/method/fungsi tidak wujud';
+		//$this->papar->baca('sesat/index');
+	}
+	
+	function paramPanjangSangat($bilParam)
+	{
+		echo '<br>class Sesat::paramPanjangSangat($bilParam = ' . $bilParam . ') extends Kawal';
+		$this->papar->mesej = 'Class wujud tapi parameter/method/fungsi tidak wujud';
+		$this->papar->baca('sesat/index');
+	}
+#--------------------------------------------------------------------------------------#
+}
