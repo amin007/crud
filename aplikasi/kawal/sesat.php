8eceb60462b9626e42687c39f856078cd4e495c8
Amin PO Muar <helmi007@yahoo.com>
2015-11-05 11:09:57 +0800

    buka komen

--- aplikasi/kawal/sesat.php
+++ aplikasi/kawal/sesat.php
@@ -12,21 +12,21 @@ class Sesat extends Kawal
 	{
 		echo '<br>class Sesat::index() extends Kawal';
 		$this->papar->mesej = 'Halaman ini tidak wujud';
-		//$this->papar->baca('sesat/index');
+		$this->papar->baca('sesat/index');
 	}
 
 	function parameter() 
 	{
 		echo '<br>class Sesat::parameter() extends Kawal';
 		$this->papar->mesej = 'Class wujud tapi parameter/method/fungsi tidak wujud';
-		//$this->papar->baca('sesat/index');
+		$this->papar->baca('sesat/index');
 	}
 	
 	function paramPanjangSangat($bilParam)
 	{
 		echo '<br>class Sesat::paramPanjangSangat($bilParam = ' . $bilParam . ') extends Kawal';
 		$this->papar->mesej = 'Class wujud tapi parameter/method/fungsi tidak wujud';
-		//$this->papar->baca('sesat/index');
+		$this->papar->baca('sesat/index');
 	}
 #--------------------------------------------------------------------------------------#
 }
