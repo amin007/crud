ed0755597a256c9e8d96c930c951f6420ac73937
Amin PO Muar <helmi007@yahoo.com>
2015-12-10 10:15:41 +0800

    tambah class Index_Tanya

---
+++ aplikasi/tanya/index_tanya.php
@@ -0,0 +1,16 @@
+<?php
+
+class Index_Tanya extends Tanya 
+{
+
+	public function __construct() 
+	{
+		parent::__construct();
+	}
+	
+	public function index()
+	{
+		echo '<br>class Test_Tanya extends Tanya<br>';
+	}
+###############################################################################################################
+}
