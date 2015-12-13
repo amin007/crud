e7ad994d95294c1979d4642e8ed33853dbd3d492
Amin PO Muar <helmi007@yahoo.com>
2015-12-13 16:15:56 +0800

    cuba fungsi baru dalam branch cubaan

--- index.php
+++ index.php
@@ -18,24 +18,47 @@ date_default_timezone_set('Asia/Kuala_Lumpur');
 # 3. setkan tatarajah sistem
 require 'tatarajah2.php';
 
-# 4. masukkan semua fail class dari folder PUSTAKA
-#    Also spl_autoload_register (Take a look at it if you like)
-function __autoload($class)
+/** 4. masukkan semua fail class dari folder PUSTAKA
+ * URL : http://www.php-fig.org/psr/psr-4/examples/
+ * An example of a project-specific implementation.
+ * 
+ * After registering this autoload function with SPL, the following line
+ * would cause the function to attempt to load the \Foo\Bar\Baz\Qux class
+ * from /path/to/project/src/Baz/Qux.php:
+ * 
+ *      new \Foo\Bar\Baz\Qux;
+ *      
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
+spl_autoload_register(function ($class) 
 {
-	$cariFail = GetMatchingFiles(GetContents('aplikasi'),$class . '.php');
-	echo '<br>Utama :: $class->' . $class . ' || $fail->' . $cariFail[0];
-	if (isset($cariFail[0])) 
-	{	require $cariFail[0];
-		if (!class_exists($class)): 
-			echo '<br>class ' . $class . ' tak wujud<br>';
-		endif;
-	}
-	else echo '<br>fail class ' . $class . ' tidak wujud <br>';
-	
-}
+    # project-specific namespace prefix
+    $prefix = 'Foo\\Bar\\'; //echo '<br>' . $prefix;
 
-# 5. istihar class Mulakan
-$aplikasi = new Mulakan();
-//require('aplikasi/pustaka/Mulakan.php'); 
-//use aplikasi\pustaka\Mulakan;
-//$aplikasi = new Mulakan('index');
+    # base directory for the namespace prefix
+    $base_dir = __DIR__ . '/pustaka/vendor/foo.bar/src/'; //echo '<br>' . $base_dir;
+
+    # does the class use the namespace prefix?
+    $len = strlen($prefix); //echo '<br>' . $len;
+    if (strncmp($prefix, $class, $len) !== 0) 
+        # no, move to the next registered autoloader
+        return;
+
+    # get the relative class name
+    $relative_class = substr($class, $len); //echo '<br>' . $relative_class;
+
+    # replace the namespace prefix with the base directory, replace namespace
+    # separators with directory separators in the relative class name, append
+    # with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+	//echo '<br>' . $file;
+    # if the file exists, require it
+    if (file_exists($file)) require $file;
+    
+});
+
+# 5. istihar class 
+new \Foo\Bar\Ayam;
+new \Foo\Bar\Baz;
+new \Foo\Bar\Qux\Quux;
